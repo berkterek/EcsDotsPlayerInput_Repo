65cdcff32748d2010c1f24292164d46fd5ef2ee6
berkterek <42406263+berkterek@users.noreply.github.com>
2023-08-27 15:39:00 +0300

    refactor: entity job and aspect added

---
+++ EcsDotsPlayerInput/Assets/Scripts/Aspects/PlayerAspect.cs
@@ -0,0 +1,30 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+namespace Aspects
+{
+    public readonly partial struct PlayerAspect : IAspect
+    {
+        public readonly Entity Entity;
+        readonly RefRO<PlayerTag> _playerTagRO;
+        readonly RefRO<InputData> _inputDataRO;
+        readonly RefRO<MoveData> _moveDataRO;
+        readonly RefRW<LocalTransform> _localTransformRW;
+
+        public void MoveProcess(float deltaTime)
+        {
+            var direction = _inputDataRO.ValueRO.Direction;
+            _localTransformRW.ValueRW.Position += deltaTime * _moveDataRO.ValueRO.MoveSpeed * direction;
+        }
+
+        public void RotationProcess()
+        {
+            var direction = _inputDataRO.ValueRO.Direction;
+            if (!direction.Equals(float3.zero))
+            {
+                _localTransformRW.ValueRW.Rotation = quaternion.LookRotation(direction, new float3(0f,1f,0f));    
+            }
+        }
+    }
+}
--- EcsDotsPlayerInput/Assets/Scripts/Systems/PlayerMoveSystem.cs
+++ EcsDotsPlayerInput/Assets/Scripts/Systems/PlayerMoveSystem.cs
@@ -1,6 +1,6 @@
-﻿using Unity.Burst;
+﻿using Aspects;
+using Unity.Burst;
 using Unity.Entities;
-using Unity.Mathematics;
 using Unity.Transforms;
 
 [BurstCompile]
@@ -20,19 +20,75 @@ public partial struct PlayerMoveSystem : ISystem
     {
         float deltaTime = SystemAPI.Time.DeltaTime;
 
-        Entity playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
-        LocalTransform localTransform = SystemAPI.GetComponent<LocalTransform>(playerEntity);
-        MoveData moveData = SystemAPI.GetComponent<MoveData>(playerEntity);
-        InputData inputData = SystemAPI.GetComponent<InputData>(playerEntity);
-
-        var direction = inputData.Direction;
-        localTransform.Position += deltaTime * moveData.MoveSpeed * direction;
-
-        if (!direction.Equals(float3.zero))
+        //Entity playerEntity = SystemAPI.GetSingletonEntity<PlayerTag>();
+        //LocalTransform localTransform = SystemAPI.GetComponent<LocalTransform>(playerEntity); //Write Read
+        //MoveData moveData = SystemAPI.GetComponent<MoveData>(playerEntity); //Readonly
+        //InputData inputData = SystemAPI.GetComponent<InputData>(playerEntity); //Readonly
+        
+        new PlayerMoveJob()
         {
-            localTransform.Rotation = quaternion.LookRotation(inputData.Direction, new float3(0f,1f,0f));    
-        } 
-
-        SystemAPI.SetComponent(playerEntity, localTransform);
+            DeltaTime = deltaTime
+        }.ScheduleParallel();
     }
 }
+
+[BurstCompile]
+public partial struct PlayerMoveJob : IJobEntity
+{
+    public float DeltaTime;
+    
+    [BurstCompile]
+    private void Execute(PlayerAspect playerAspect)
+    {
+       playerAspect.MoveProcess(DeltaTime);
+    }
+}
+
+[BurstCompile]
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+[UpdateBefore(typeof(TransformSystemGroup))]
+[UpdateAfter(typeof(PlayerMoveSystem))]
+public partial struct PlayerRotationSystem : ISystem
+{
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        float deltaTime = SystemAPI.Time.DeltaTime;
+
+        new PlayerRotationJob()
+        {
+            DeltaTime = deltaTime
+        }.ScheduleParallel();
+    }
+}
+
+[BurstCompile]
+public partial struct PlayerRotationJob : IJobEntity
+{
+    public float DeltaTime;
+    
+    [BurstCompile]
+    private void Execute(PlayerAspect playerAspect)
+    {
+        playerAspect.RotationProcess();
+    }
+}
+
+//first example
+// [BurstCompile]
+// public partial struct PlayerMoveJob : IJobEntity
+// {
+//     public float DeltaTime;
+//     
+//     [BurstCompile]
+//     private void Execute(Entity entity, ref LocalTransform localTransform, in MoveData moveData, in InputData inputData)
+//     {
+//         var direction = inputData.Direction;
+//         localTransform.Position += DeltaTime * moveData.MoveSpeed * direction;
+//
+//         if (!direction.Equals(float3.zero))
+//         {
+//             localTransform.Rotation = quaternion.LookRotation(inputData.Direction, new float3(0f,1f,0f));    
+//         } 
+//     }
+// }
